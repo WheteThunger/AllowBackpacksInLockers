017ec8c473f1dc77365b6eaa8c3f5f3a55843853
WhiteThunder <whetethunger@gmail.com>
2024-02-05 17:58:21 -0800

    Initial commit

---
+++ AllowBackpacksInLockers.cs
@@ -0,0 +1,21 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Allow Backpacks in Lockers", "WhiteThunder", "1.0.0")]
+    [Description("Allows players to store non-empty backpacks in lockers.")]
+    internal class AllowBackpacksInLockers : CovalencePlugin
+    {
+        private static readonly object CanAccept = ItemContainer.CanAcceptResult.CanAccept;
+
+        private object CanAcceptItem(ItemContainer container, Item item, int targetPosition)
+        {
+            var locker = container.entityOwner as Locker;
+            if (locker == null
+                || locker.inventory != container
+                || !item.IsBackpack()
+                || !locker.IsBackpackSlot(targetPosition))
+                return null;
+
+            return CanAccept;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Features
+
+Allows players to store non-empty vanilla backpacks in lockers
+
+No config or permissions.
